5f128b230c40ef993d1f4df671dbb93476a9530a
Ahmet Karakaya <karakayahmet00@gmail.com>
2022-10-16 17:28:56 +0300

    scatter, histogram, step ve piechart gösterimi tamamlandı.

--- scatter-histogram-step-piechart.py
+++ scatter-histogram-step-piechart.py
@@ -3,6 +3,7 @@ from cmath import rect
 from turtle import color
 from matplotlib import markers
 from matplotlib.lines import lineStyles
+from numpy import size
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -10,20 +11,27 @@ x = ["1.ABD","2.Çin","3.Japonya","4.Almanya","5.İngiltere","6.Fransa"]
 y = [12342,12334,23449,24234,31343,24324]
 z = [11234,12344,43221,53223,53423,43221]
 
-f = plt.figure(figsize=(7,3))
+f = plt.figure()
 axes = f.add_axes([0.1,0.1,0.9,0.9])
-axes.plot(x,y,"r")
+
+#scatter
+#axes.scatter(x,y)
+
+#histogram
+#axes.hist(y)
+
+#step
+#axes.step(x,y)
 axes.set_xlabel("Ülkeler")
 axes.set_ylabel("Milli Gelir")
 axes.set_title("Ülkelerine Göre Milli Gelir")
 
-ax = f.add_axes([0,0,1,1])
-ax.plot(x,y,"r", label="milli gelir")
-ax.plot(x,z,"y", label="kişi başına")
+labels = ["Türkiye","ABD","Çin","Almanya"]
+sizes = [35,25,23,17]
+explode = (0.1,0,0,0)
 
-ax.legend(loc=1)
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels)
 
-f.savefig("resim.png")
+
 plt.show()
-
-
